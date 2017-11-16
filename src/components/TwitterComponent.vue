e44356a7f838511802a467c39fab00783689268f
mmck328 <edamame1010@hotmail.co.jp>
2017-11-16 17:21:12 +0900

    Update TwitterComponent

--- src/components/TwitterComponent.vue
+++ src/components/TwitterComponent.vue
@@ -5,17 +5,27 @@
 </template>
 
 <script>
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+!(function (d, s, id) {
+  var js
+  var fjs = d.getElementsByTagName(s)[0]
+  var p = /^http:/.test(d.location) ? 'http' : 'https'
+  if (!d.getElementById(id)) {
+    js = d.createElement(s)
+    js.id = id
+    js.src = p + '://platform.twitter.com/widgets.js'
+    fjs.parentNode.insertBefore(js, fjs)
+  }
+})(document, 'script', 'twitter-wjs')
 
 export default {
   name: 'TwitterComponent',
   data () {
-    return {
-    }
-  },
+    return {}
+  }
 }
 </script>
 
 <!-- Add "scoped" atribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
 </style>
