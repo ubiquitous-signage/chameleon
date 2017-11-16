0da92024ea252858fce2a87a009ee15091d30387
mmck328 <edamame1010@hotmail.co.jp>
2017-11-16 17:45:11 +0900

    Update twitter style

--- src/components/TwitterComponent.vue
+++ src/components/TwitterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="twitter">
-    <a class="twitter-timeline"  href="https://twitter.com/search?q=%E3%83%80%E3%82%A4%E3%83%AF%E3%83%A6%E3%83%93%E3%82%AD%E3%82%BF%E3%82%B9%E5%AD%A6%E8%A1%93%E7%A0%94%E7%A9%B6%E9%A4%A8" data-widget-id="930820136956715008">ダイワユビキタス学術研究館に関するツイート</a>
+    <a class="twitter-timeline"  href="https://twitter.com/search?q=%E3%83%80%E3%82%A4%E3%83%AF%E3%83%A6%E3%83%93%E3%82%AD%E3%82%BF%E3%82%B9%E5%AD%A6%E8%A1%93%E7%A0%94%E7%A9%B6%E9%A4%A8" data-widget-id="930820136956715008" data-chrome="noheader nofooter transparent noborders noscrollbar">ダイワユビキタス学術研究館に関するツイート</a>
   </div>
 </template>
 
@@ -27,5 +27,8 @@ export default {
 
 <!-- Add "scoped" atribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
+.twitter {
+  margin: 0 0 -1rem 0;
+  padding: 0;
+}
 </style>
