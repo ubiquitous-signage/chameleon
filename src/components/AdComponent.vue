21754c93e45c2c6dbe6e80a22fdbe8c749a49dae
mmck328 <edamame1010@hotmail.co.jp>
2017-12-03 03:25:48 +0900

    Update  AdComponent style

--- src/components/AdComponent.vue
+++ src/components/AdComponent.vue
@@ -3,6 +3,10 @@
     <div class="panel" v-for="item in items" :key="item.title.ja">
       <panel-component :lang="lang" :version="item.version" :type="item.type" :title="item.title" :contents="item.contents"></panel-component>
     </div>
+
+    <!-- space-betweenで最終行を左揃えするための空要素 -->
+    <div class="dummyPanel"></div>
+    <div class="dummyPanel"></div>
   </div>
 </template>
 
@@ -24,10 +28,17 @@ export default {
   overflow: visible;
   display: flex;
   align-content: flex-start;
+  align-items: baseline;
+  justify-content: space-between;
   flex-wrap: wrap;
   .panel {
     flex-basis: 31.4%;
     padding: 10px;
+  }
+  .dummyPanel {
+    flex-basis: 31.4%;
+    margin: 0;
+    padding: 0;
   }
 }
 </style>
