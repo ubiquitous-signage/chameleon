c4d5cfce66db96b8551fc1997c4d1fd60222121f
mmck328 <edamame1010@hotmail.co.jp>
2017-12-26 09:37:00 +0900

    Fix AboutComponent

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -3,7 +3,7 @@
     <h2>
       {{ this.title[lang] }}
     </h2>
-    <div class="paragraphs">
+    <div class="figure">
       <img v-if="lang==='ja'" src="../assets/about_ja.png">
       <img v-if="lang==='en'" src="../assets/about_en.png">
     </div>
@@ -33,22 +33,34 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .about {
+  background: white;
   text-align: left;
-  font-size: 3.5rem;
-  padding-left: 5rem;
-  padding-right: 5rem;
-  height: 1770px;
+  box-sizing: border-box;
+  padding: 10%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
 }
 
 .about h2 {
-  font-size: 3.5rem;
+  font-size: 5rem;
+  text-align: center;
+}
+
+.figure {
+  display: table-cell;
+  vertical-align: middle;
+  height: 1500px;
 }
 
 img {
   display: block;
   width: 100%;
   margin: 0;
-  margin-top: 3vh;
+  padding: 0;
+  margin-top: 5vh;
 }
 
 </style>
